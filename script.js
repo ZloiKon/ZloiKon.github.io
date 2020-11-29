ae362a2811f9d1a95f1c08d65787691e58be19c0
ZloiKon <maks.miron0v@yandex.ru>
2020-11-29 14:47:57 +0300

    пуш для внедрения сообщения

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
+
+
+
+    $(".modal-body").load("https://artert241.github.io a.card-text.text-center", function (response, status, request) {
+        
+        
+    });
 });
 
 
+$("btnSelect").click(function (e) {
+    console.log($("option[selected='true']"));
+});
 
-
-
+Ок
 
 
 
