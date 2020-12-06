fd36352af8d146913428886b5eb4ba07578d850c
ZloiKon <maks.miron0v@yandex.ru>
2020-12-06 15:00:56 +0300

    33

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
 $("#btnSelect").click(function (e) {
     let select = document.querySelector("select")
     let url = select.options[select.selectedIndex].value + " " + "div#answer";
-    document.querySelector("#msgLabel").innerHTML = "Сообщение от фраера " + select.options[select.selectedIndex].innerHTML;
+    document.querySelector("#msgLabel").innerHTML = " Сообщение от фраера  " + select.options[select.selectedIndex].innerHTML;
 
     $(".modal-body").load(url, function (response, status, requwst) {
-        document.querySelector("modal-body>div").id = "";
+        document.querySelector(".modal-body>div").id = "";
         $("#msgBox").modal("show");
     });
 });  
